5da54089d7ab36d977c0447931a254928f868023
JordanJ34 <Fanderobert@hotmail.com>
2025-04-14 14:45:02 +0200

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-black text-white flex items-center justify-center text-center p-10">
+      <div>
+        <h1 className="text-4xl font-bold mb-4">Les Mille de Mars</h1>
+        <p className="text-lg text-gray-400">Un site en construction, porté par l'imaginaire d'Élise sur Mars.</p>
+      </div>
+    </div>
+  );
+}
